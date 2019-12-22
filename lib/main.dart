627eba9c61f5a318515676daf3fd6d6ee5c2f0af
engida32 <enguwerk@outlook.com>
2019-12-22 13:42:49 +0300

    test3

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(bottomNavigation());
+
 class bottomNavigation extends StatefulWidget {
   bottomNavigation({Key key}) : super(key: key);
 
@@ -12,32 +13,48 @@ class bottomNavigation extends StatefulWidget {
 class _bottomNavigationState extends State<bottomNavigation> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text("BottomNav",style: TextStyle(color: Colors.black)),
-        
-      ),
-      body: Container(
-        color: Colors.deepOrange,
-   
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Bottom Navigation ",style: TextStyle(
-              color: Colors.white
-            ),)
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+    
+        appBar: AppBar(
+          backgroundColor: Colors.green[200],
+      
+          title: Text("BottomNav", style: TextStyle(color: Colors.black)),
+          centerTitle: true,
         ),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        items: <Widget>[
-          Icon(Icons.school,size: 20,color: Colors.black),
-          Icon(Icons.school,size: 20,color: Colors.black),
-          Icon(Icons.school,size: 20,color: Colors.black),
-        ],
+        body: Container(
+          color: Colors.amberAccent,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Bottom Navigation ",
+                  style: TextStyle(color: Colors.black,fontSize: 33),
+                )
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          color: Colors.greenAccent,
+          backgroundColor: Colors.amberAccent,
+          buttonBackgroundColor: Colors.greenAccent,
+          height: 65,
+          items: <Widget>[
+            Icon(Icons.school, size: 20, color: Colors.black),
+            Icon(Icons.add_box, size: 20, color: Colors.black),
+            Icon(Icons.compare_arrows, size: 20, color: Colors.black),
+            Icon(Icons.exit_to_app, size: 20, color: Colors.black),
+            Icon(Icons.favorite, size: 20, color: Colors.black),
+
+          ],
+          onTap: (index) {
+            print('u are on the $index icon ');
+          },
+        ),
       ),
     );
   }
 }
-
